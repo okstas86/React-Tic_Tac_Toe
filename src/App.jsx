7113a99c3c0cd8b611e1d990fa89dd0574e27272
okstas86 <stanislavocunev1@gmail.com>
2024-01-12 10:12:48 +0200

    Changing the active player's state control to calculation via the deriveActivePlayer function

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,22 @@ import GameBoard from './components/GameBoard'
 import Player from './components/Player'
 import Log from './components/Log'
 
+function deriveActivePlayer(gameTurns) {
+	let curentPlayer = 'X'
+	if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+		curentPlayer = 'O'
+	}
+	return curentPlayer
+}
+
 function App() {
-	const [activePlayer, setActivePlayer] = useState('X')
 	const [gameTurns, setGameTurns] = useState([])
+	const activePlayer = deriveActivePlayer(gameTurns)
 
 	function handleSelectSquare(rowIndex, colIndex) {
-		setActivePlayer(curActivePlayer => (curActivePlayer === 'X' ? 'O' : 'X'))
 		setGameTurns(prevTurns => {
-			let curentPlayer = 'X'
-			if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-				curentPlayer = 'O'
-			}
+			let curentPlayer = deriveActivePlayer(prevTurns)
+
 			const updatedTurns = [
 				{
 					square: {
@@ -24,6 +29,7 @@ function App() {
 				},
 				...prevTurns,
 			]
+
 			return updatedTurns
 		})
 	}
