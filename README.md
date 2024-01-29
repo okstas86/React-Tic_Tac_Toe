22b49d9efb97702b0774c6323bed8d4b52a010e9
okstas86 <stanislavocunev1@gmail.com>
2024-01-29 21:26:25 +0200

    Cleaned app component

---
+++ README.md
@@ -0,0 +1,22 @@
+# Thr game Tic_Tac_Toe is built with React
+
+<img align="left" alt="screen" width="800" src="./public/main.png" />
+<img align="left" alt="screen" width="800" src="./public/game_over.png" />
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,42 @@
-import { useState } from 'react'
-import GameBoard from './components/GameBoard'
-import GameOver from './components/GameOver'
-import Player from './components/Player'
-import Log from './components/Log'
-import { WINNING_COMBINATIONS } from './winning-combination'
+import { useState } from "react"
+import GameBoard from "./components/GameBoard"
+import GameOver from "./components/GameOver"
+import Player from "./components/Player"
+import Log from "./components/Log"
+import { WINNING_COMBINATIONS } from "./winning-combination"
 
-const initialGameBoard = [
+const PLAYERS = {
+	X: "Player 1",
+	O: "Player 2",
+}
+
+const INITIAL_GAME_BOARD = [
 	[null, null, null],
 	[null, null, null],
 	[null, null, null],
 ]
 
 function deriveActivePlayer(gameTurns) {
-	let curentPlayer = 'X'
-	if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
-		curentPlayer = 'O'
+	let curentPlayer = "X"
+
+	if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+		curentPlayer = "O"
 	}
 	return curentPlayer
 }
 
-function App() {
-	const [gameTurns, setGameTurns] = useState([])
-	const [players, setPlayers] = useState({
-		X: 'Player 1',
-		O: 'Player 2',
-	})
-	const activePlayer = deriveActivePlayer(gameTurns)
-
-	let gameBoard = [...initialGameBoard.map(array => [...array])]
+function deriveGameBoard(gameTurns) {
+	let gameBoard = [...INITIAL_GAME_BOARD.map(array => [...array])]
 
 	for (const turn of gameTurns) {
 		const { square, player } = turn
 		const { row, col } = square
 		gameBoard[row][col] = player
 	}
+	return gameBoard
+}
 
+function deriveWinner(gameBoard, players) {
 	let winner = null
 
 	for (const combination of WINNING_COMBINATIONS) {
@@ -53,6 +55,16 @@ function App() {
 			winner = players[firstSquareSimbol]
 		}
 	}
+	return winner
+}
+
+function App() {
+	const [players, setPlayers] = useState(PLAYERS)
+	const [gameTurns, setGameTurns] = useState([])
+
+	const activePlayer = deriveActivePlayer(gameTurns)
+	const gameBoard = deriveGameBoard(gameTurns)
+	const winner = deriveWinner(gameBoard, players)
 	const hasDraw = gameTurns.length === 9 && !winner
 
 	function handleSelectSquare(rowIndex, colIndex) {
@@ -91,15 +103,15 @@ function App() {
 			<div id='game-container' className='highlight-player'>
 				<ol id='players' className='highlight-player'>
 					<Player
-						initialName={'Player 1'}
-						symbol={'X'}
-						isActive={activePlayer === 'X'}
+						initialName={PLAYERS.X}
+						symbol={"X"}
+						isActive={activePlayer === "X"}
 						onChangeName={handlePlayerNameChange}
 					/>
 					<Player
-						initialName={'Player 2'}
-						symbol={'O'}
-						isActive={activePlayer === 'O'}
+						initialName={PLAYERS.O}
+						symbol={"O"}
+						isActive={activePlayer === "O"}
 						onChangeName={handlePlayerNameChange}
 					/>
 				</ol>
